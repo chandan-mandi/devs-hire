a3e6ba2bbf574ae7696b4a8cca990422d8f118f8
chandan-mandi <itscoderchandan@gmail.com>
2021-09-26 21:55:54 +0530

    hover effect update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Header.css';
 const Header = () => {
     return (
-        <div className="container header-section">
+        <div className="header-section">
             <h1>Make a Developer Team</h1>
             <p>“Start with something simple and small, then expand over time. If people call it a ‘toy’ you’re definitely onto something.”</p>
             <h1>Total Budget: 100 Million</h1>
